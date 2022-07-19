f2f49e57b542a6ad34ca4585643b3f2b319ad1cb
Veronica Andersen <veronica.andersen@se.experis.com>
2022-07-19 20:37:13 +0200

    Installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{ts,html}" // Adds tailwindcss to all files in src with .ts or .html
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
