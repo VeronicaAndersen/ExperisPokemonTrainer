b62f0804e8c7ead55ba3b23524b1203743fc6476
Veronica Andersen <veronica.andersen@se.experis.com>
2022-07-20 09:57:33 +0200

    Feat(LandingPage): Setup services

---
+++ src/app/services/landing.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LandingService {
+
+  constructor() { }
+
+  // Models, Observables, and RxJS operators.
+
+  // login
+
+  // Check if user exist
+
+  // If not user - create User
+
+  // If user || Create User -> Store User
+}
