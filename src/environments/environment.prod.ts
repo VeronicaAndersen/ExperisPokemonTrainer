7583eb051bc3f1368334da96f2d586d0ec86dfa6
Veronica Andersen <veronica.andersen@se.experis.com>
2022-07-22 11:25:15 +0200

    Fix: styling Card and navbar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUsers: "https://assignmenttwo-api.herokuapp.com/trainers",
-  apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=10/", // "https://pokeapi.co/api/v2/pokemon",
+  apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=20/", // "https://pokeapi.co/api/v2/pokemon",
   apiKey: "xcozeknGOUiRqMwiw4kwag=="
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: false,
   apiUsers: "https://assignmenttwo-api.herokuapp.com/trainers",
-  apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=10/", // "https://pokeapi.co/api/v2/pokemon",
+  apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=20/", // "https://pokeapi.co/api/v2/pokemon",
   apiKey: "xcozeknGOUiRqMwiw4kwag=="
 };
 
