9c24c25bb1263700711ba0d51291fd92fec274bc
Veronica Andersen <veronica.andersen@se.experis.com>
2022-07-25 15:48:40 +0200

    Fix: for deployment

--- src/environments/environments.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    apiUsers: "https://assignmenttwo-api.herokuapp.com/trainers",
-    apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=50/", // 
-    apiKey: "xcozeknGOUiRqMwiw4kwag=="
-};
