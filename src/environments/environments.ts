b784324088512c6235848e3444dc2de867f22f53
Veronica Andersen <veronica.andersen@se.experis.com>
2022-07-25 14:09:55 +0200

    DEPLOY: Heroku buildpack

---
+++ src/environments/environments.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUsers: "https://assignmenttwo-api.herokuapp.com/trainers",
+    apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=50/", // 
+    apiKey: "xcozeknGOUiRqMwiw4kwag=="
+};
