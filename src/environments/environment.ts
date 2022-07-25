2f07870e3a21071bf09ffb1a5dd1b16d7e39be48
Johanna Olsson <johanna.olsson@se.experis.com>
2022-07-25 11:07:18 +0200

    Fix: deleted environments files

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiUsers: "https://assignmenttwo-api.herokuapp.com/trainers",
-  apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=50/", 
-  apiKey: "xcozeknGOUiRqMwiw4kwag=="
-};
-
